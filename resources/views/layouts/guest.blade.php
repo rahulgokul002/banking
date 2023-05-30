cd31c516603ae52a438558737cb22cb43f9c3d37
rahulraj <rahulgokul002@gmail.com>
2023-05-30 12:39:19 +0530

    'Added'

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,10 +12,11 @@
 
         <!-- Scripts -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <script src="{{ asset('js/app.js') }}" defer></script>    </head>
+        <script src="{{ asset('js/app.js') }}" defer></script>  
+      </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
         </div>
-    </body>
+     </body>
 </html>
